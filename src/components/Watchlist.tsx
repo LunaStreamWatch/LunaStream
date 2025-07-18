4b50192652c9daef98263ff93d42ade807b58e5a
Aqua Quokka <AquaQuokka@outlook.com>
2025-07-18 19:21:40 +0800

    remove src

--- src/components/Watchlist.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-// Redirect to Vault since we've combined them
-const Watchlist: React.FC = () => {
-  return <Navigate to="/vault" replace />;
-};
-
-export default Watchlist;
